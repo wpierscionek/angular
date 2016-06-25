3267a01451ed95d755b79c659dcd12dfb9773d6a
wpierscionek <pierscionekW@gmail.com>
2016-06-24 22:12:26 -0400

    controller

---
+++ components/classifieds.ctr.js
@@ -0,0 +1,15 @@
+// Setting up controller
+
+
+(function(){
+
+	"use strict"; 
+// Referencing the module we already created
+	angular
+	.module("ngClassifieds")
+// Creating controller method
+	.controller("ngClassifiedsCtrl", function(){
+// Controller is now tied to our view in HTML by using 'ng-controller="classifiedsCtrl'
+	
+	});
+})();
