ec6b562aa37a95b8b5f39af6c03b70263ef3984e
wpierscionek <pierscionekW@gmail.com>
2016-06-24 22:28:10 -0400

    controller

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -7,9 +7,14 @@
 // Referencing the module we already created
 	angular
 	.module("ngClassifieds")
-// Creating controller method
-	.controller("ngClassifiedsCtrl", function(){
+// Creating controller method and injecting $scope into parameters into annonymous function asociated with controller
+	.controller("classifiedsCtrl", function($scope){
 // Controller is now tied to our view in HTML by using 'ng-controller="classifiedsCtrl'
-	
+// $scope is a special object thats available to us in controllers, and its the glue between controllers and view
+	$scope.name = {
+		first: "Rua",
+		last: "fdfs"
+	}
+
 	});
 })();
